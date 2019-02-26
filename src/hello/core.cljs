e731f1bacd13e4b8a906740b02d2bb0beaaf7b62
Danyele <danyele@Mac-mini-de-Danyele.local>
2019-02-26 14:13:33 -0300

    renderizando filme selecionado

--- src/hello/core.cljs
+++ src/hello/core.cljs
@@ -1,25 +1,45 @@
 (ns hello.core
   (:require [reagent.core :as reagent :refer [atom]]))
 
+
+
 (def catalogo-filmes [
-                      {:id 1
+                      {
+                       :id 1
                        :nome "Lady gaga" 
+                       :video "https://i.ytimg.com/vi/WGsA2aXYBGo/maxresdefault.jpg"
                        :capa "https://m.media-amazon.com/images/M/MV5BNmE5ZmE3OGItNTdlNC00YmMxLWEzNjctYzAwOGQ5ODg0OTI0XkEyXkFqcGdeQXVyMTMxODk2OTU@._V1_UX182_CR0,0,182,268_AL_.jpg"}
                       {
                        :id 2
                        :nome "Fredy mercury"
+                       :video "https://s2.glbimg.com/lMC9uMH29_s0Tleemgr_nl4vUQ0=/e.glbimg.com/og/ed/f/original/2018/11/14/bohemian-rhapsody3.jpg"
                        :capa "https://m.media-amazon.com/images/M/MV5BNDg2NjIxMDUyNF5BMl5BanBnXkFtZTgwMzEzNTE1NTM@._V1_UX182_CR0,0,182,268_AL_.jpg"}
                       {
                        :id 3
                        :nome "Rei leão"
+                       :video "https://media.hugogloss.uol.com.br/uploads/2019/02/o-rei-leao-234892394-900x350.jpg"
                        :capa "https://m.media-amazon.com/images/M/MV5BMjIwMjE1Nzc4NV5BMl5BanBnXkFtZTgwNDg4OTA1NzM@._V1_UX182_CR0,0,182,268_AL_.jpg"}
                       ])
-(defn filme-selecionado [filme]
-  [:div {:style {:background-color :red}}[:p (:nome filme)]])
+
+
+(defonce estado-do-filme-selecionado (reagent/atom nil))
+
+
+(defn renderizar-filme-selecionado [filme]
+  [:div {:style {:background-color :purple
+                 :padding 10}}
+   [:h3 {:style {:color "#ffffff"
+                 :text-align :center}} (:nome filme)]
+   [:img {
+          :style {:width "100%"}
+          :src (:video filme)}]])
+
+(defn selecionar-filme [filme]
+  (reset! estado-do-filme-selecionado filme))
 
 
 (defn chamada-do-filme [filme]
- [:div { :on-click #(js/alert "pegou")
+ [:div { :on-click #(selecionar-filme filme)
         :style {:margin 10}}
   [:img {:src (get filme :capa) 
          :style {:width 150}
@@ -28,12 +48,13 @@
                :text-align "center"}}(get filme :nome)]] )
 
 (defn netflix []
-  (println catalogo-filmes)
+
   [:div 
    [:div {:style {:display "flex"
                   :justify-content "left"}} (map chamada-do-filme catalogo-filmes)
     ]
-   [filme-selecionado (first catalogo-filmes)]])
+   [renderizar-filme-selecionado @estado-do-filme-selecionado]])
+
 
 
 
